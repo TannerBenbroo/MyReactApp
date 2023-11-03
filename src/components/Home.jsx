945b70a458df0775554e70205b88fcbec8de0b60
Tanner Benbrook <Tanner.M.Benbrook-1@ou.edu>
2023-11-02 22:14:54 -0500

    cock

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,43 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import { NavBar } from './NavBar';
 import { HomeWrapper, MainContent, Greeting, Hobbies, SocialLinks, Avatar} from './HomeStyledComponents';
 
+const SocialLink = (props) => {
+    console.log(props.url); // This will print the url prop to the console
+
+    return (
+        <a target="_blank" rel="noopener noreferrer" href={props.url}>
+            {props.children}
+        </a>
+    );
+}
+
+const funFacts = ['React enthusiast' , 'CS student' , 'OU 2025'];
+
+const funFactsBuilder = (length = 3) => {
+    const facts = funFacts
+        .map(fact => ({ fact, randPos: Math.random()}))
+        .sort((a,b) => a.randPos - b.randPos)
+        .map(factObject => factObject.fact)
+        .slice(0, length)
+        .join(' | ')
+
+    return facts.toString();
+}
+
 export const Home = () => (
-    <div style={{color: 'black', padding: '10px', fontSize: '48px'}}>
-        HOME
-    </div>
+    <HomeWrapper>
+        <MainContent>
+            <Avatar picture="images/Wravis-Wcott.jpg" />
+            <Greeting>Yo, this is Tanner</Greeting>
+            <Hobbies>{funFactsBuilder()}</Hobbies>
+            <SocialLinks>
+                <SocialLink url = "https://www.instagram.com/phocisticks/">
+                    <InstagramIcon style={{ fontSize: 40 }} />
+                </SocialLink>
+                <SocialLink url = "https://github.com/TannerBenbroo">
+                    <GitHubIcon style= {{ fontSize: 40}} />
+                </SocialLink>
+            </SocialLinks>
+        </MainContent>
+    </HomeWrapper>
 );
--- src/components/HomeStyledComponents.jsx
+++ src/components/HomeStyledComponents.jsx
@@ -1,7 +1,7 @@
 import Styled from '@emotion/styled';
 
 export const HomeWrapper = Styled.div`
-    background-color: darkGreen;
+    background-color: gray;
     background-size: cover;
     height: 100%;
     overflow: hidden;
