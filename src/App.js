a780dc230309d09ed9a5156827d123c102d2b08e
Tanner Benbrook <Tanner.M.Benbrook-1@ou.edu>
2023-11-02 21:50:44 -0500

    push da whole bih

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Resume } from './components/Resume';
+import { Home } from './components/Home';
+
+const App = () => (
+  <div style={{ color: 'black', padding: '10px', fontSize: '48px' }}>
+    APP
+  </div>
+);
+
+
+
+
+export default App;
